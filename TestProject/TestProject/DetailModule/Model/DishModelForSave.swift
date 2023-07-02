2813888fb19fd2a4ee25a1ef6eeda95321756896
Novgorodtcev <82874611+Berendei75405@users.noreply.github.com>
2023-07-03 01:48:46 +0700

    Add files via upload

---
+++ TestProject/TestProject/DetailModule/Model/DishModelForSave.swift
@@ -0,0 +1,19 @@
+//
+//  DishModelForSave.swift
+//  TestProject
+//
+//  Created by user on 03.07.2023.
+//
+
+import Foundation
+
+// MARK: - DishModelForSave
+struct DishModelForSave: Codable {
+    let name: String
+    let price, weight: Int
+    let image: Data
+
+    enum CodingKeys: String, CodingKey {
+        case name, price, weight, image
+    }
+}
