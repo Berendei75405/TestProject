8cd8e09e7947d3613fc19d070ba7393487176b11
Novgorodtcev <82874611+Berendei75405@users.noreply.github.com>
2023-07-03 01:50:06 +0700

    Update DishViewModel.swift

--- TestProject/TestProject/DetailModule/ViewModel/DishViewModel.swift
+++ TestProject/TestProject/DetailModule/ViewModel/DishViewModel.swift
@@ -6,15 +6,103 @@
 //
 
 import Foundation
+import Combine
 
-protocol DishViewModelProtocol {
-    var dishArray: [Dish] {get set}
+protocol DishViewModelProtocol: AnyObject {
+    var dishArray: DishArray? {get set}
+    var imageArray: [Int : Data] {get set}
     var coordinator: MainCoordinatorProtocol! {get}
+    var updateTableState: PassthroughSubject<Bool, Never> {get set}
+    var sortDescriptor: Teg {get set}
+    var titleArray: [String] {get}
+    func fetchDish()
+    func getDishSorted() -> DishArray
+    func appendDish(dish: Dish, image: Data, key: String)
 }
 
 class DishViewModel: DishViewModelProtocol {
-    var vc: DishViewController!
-    var dishArray: [Dish] = []
+    var dishArray: DishArray?
+    var imageArray: [Int : Data] = [:]
     var coordinator: MainCoordinatorProtocol!
+    var updateTableState = PassthroughSubject<Bool, Never>()
+    var sortDescriptor: Teg = .всеМеню { didSet {self.updateTableState.send(true)}}
+    let titleArray = ["Все меню", "Салаты", "С рисом", "С рыбой", "Роллы"]
+    private var cancellable = Set<AnyCancellable>()
+    
+    //MARK: - fetchDish
+    func fetchDish() {
+        guard let url = URL(string: "https://run.mocky.io/v3/aba7ecaa-0a70-453b-b62d-0e326c859b3b") else {return}
+        let request = URLRequest(url: url)
+        
+        //издатель
+        let publisher = URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: DishArray.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+        
+        publisher.sink { completion in
+            switch completion {
+            case .finished:
+                print("Fetch categories pulisher was finished!")
+            case .failure(let error):
+                print(error)
+            }
+        } receiveValue: { [self] result in
+            self.dishArray = result
+            getImage()
+        }.store(in: &cancellable)
+    }
+    
+    //MARK: - getImage
+    private func getImage() {
+        guard let dishArray = dishArray?.dishes else {return}
+        
+        for item in dishArray {
+            guard let url = URL(string: item.imageURL) else {return}
+            
+            let publisher = URLSession.shared.dataTaskPublisher(for: url)
+                .map(\.data)
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+            
+            publisher.sink { completion in
+                switch completion {
+                case .finished:
+                    print("image \(item.id) was winished")
+                case .failure(let error):
+                    print(error)
+                }
+            } receiveValue: { [unowned self] result in
+                self.imageArray[item.id] = result
+                if self.imageArray.count == self.dishArray?.dishes.count {
+                    updateTableState.send(true)
+                }
+            }.store(in: &cancellable)
+        }
+    }
+    
+    //MARK: - getDishSorted
+    func getDishSorted() -> DishArray {
+        var dishSort: DishArray = .init(dishes: [])
+        
+        if let dishArray = dishArray?.dishes {
+            for item in dishArray {
+                for j in item.tegs {
+                    if j == sortDescriptor {
+                        dishSort.dishes.append(item)
+                    }
+                }
+            }
+        }
+        return dishSort
+    }
+    
+    //MARK: - appendDish
+    func appendDish(dish: Dish, image: Data, key: String) {
+        let dishModelForSave = DishModelForSave(name: dish.name, price: dish.price, weight: dish.price, image: image)
+        let userDefaults = UserDefaults.standard
+        userDefaults.save(array: [dish], forKey: key)
+    }
     
 }
