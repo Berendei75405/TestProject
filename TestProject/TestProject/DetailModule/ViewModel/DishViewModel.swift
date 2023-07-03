2951f7b557285fc464c3ad860bce7c31c6646b62
Novgorodtcev <82874611+Berendei75405@users.noreply.github.com>
2023-07-03 18:24:10 +0700

    Update DishViewModel.swift

--- TestProject/TestProject/DetailModule/ViewModel/DishViewModel.swift
+++ TestProject/TestProject/DetailModule/ViewModel/DishViewModel.swift
@@ -1,121 +1,132 @@
 //
-//  DishViewModel.swift
+//  BasketViewController.swift
 //  TestProject
 //
-//  Created by user on 28.06.2023.
+//  Created by user on 02.07.2023.
 //
 
-import Foundation
+import UIKit
 import Combine
 
-protocol DishViewModelProtocol: AnyObject {
-    var dishArray: DishArray? {get set}
-    var imageArray: [Int : Data] {get set}
-    var coordinator: MainCoordinatorProtocol! {get}
-    var updateTableState: PassthroughSubject<Bool, Never> {get set}
-    var sortDescriptor: Teg {get set}
-    var titleArray: [String] {get}
-    func fetchDish()
-    func getDishSorted() -> DishArray
-    func appendDish(dish: Dish, image: Data, key: String)
-}
-
-class DishViewModel: DishViewModelProtocol {
-    var dishArray: DishArray?
-    var imageArray: [Int : Data] = [:]
-    var coordinator: MainCoordinatorProtocol!
-    var updateTableState = PassthroughSubject<Bool, Never>()
-    var sortDescriptor: Teg = .всеМеню { didSet {self.updateTableState.send(true)}}
-    let titleArray = ["Все меню", "Салаты", "С рисом", "С рыбой", "Роллы"]
+class BasketViewController: UIViewController {
+    var viewModel: BasketViewModelProtocol!
+    
+    //MARK: - tableView
+    var tableView: UITableView = {
+        var table = UITableView(frame: .zero, style: .plain)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorStyle = .none
+        table.backgroundColor = .white
+        
+        table.register(BasketCell.self, forCellReuseIdentifier: BasketCell.identifier)
+        
+        return table
+    }()
+    
+    //MARK: - buyButton
+    private var buyButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Оплатить 2 004 ₽", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2544889748, green: 0.4875386357, blue: 0.9033587575, alpha: 1)
+        button.tintColor = .white
+        
+        return button
+    }()
+    
+    //MARK: - cancellable
     private var cancellable = Set<AnyCancellable>()
     
-    //MARK: - fetchDish
-    func fetchDish() {
-        guard let url = URL(string: "https://run.mocky.io/v3/aba7ecaa-0a70-453b-b62d-0e326c859b3b") else {return}
-        let request = URLRequest(url: url)
-        
-        //издатель
-        let publisher = URLSession.shared.dataTaskPublisher(for: request)
-            .map(\.data)
-            .decode(type: DishArray.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-        
-        publisher.sink { completion in
-            switch completion {
-            case .finished:
-                print("Fetch categories pulisher was finished!")
-            case .failure(let error):
-                print(error)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBarMain()
+        setContraints()
+        updateButton()
+        configurateButton()
+    }
+    
+    //MARK: - updateButton
+    private func updateButton() {
+        self.viewModel.reloadView.sink { [weak self] bool in
+            if bool == true {
+                self?.configurateButton()
+            } else {
+                self?.configurateButton()
+                self?.tableView.reloadData()
             }
-        } receiveValue: { [self] result in
-            self.dishArray = result
-            getImage()
         }.store(in: &cancellable)
     }
     
-    //MARK: - getImage
-    private func getImage() {
-        guard let dishArray = dishArray?.dishes else {return}
+    //MARK: - setContraints
+    private func setContraints() {
+        view.backgroundColor = .white
+        self.view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
-        for item in dishArray {
-            guard let url = URL(string: item.imageURL) else {return}
-            
-            let publisher = URLSession.shared.dataTaskPublisher(for: url)
-                .map(\.data)
-                .receive(on: DispatchQueue.main)
-                .eraseToAnyPublisher()
-            
-            publisher.sink { completion in
-                switch completion {
-                case .finished:
-                    print("image \(item.id) was winished")
-                case .failure(let error):
-                    print(error)
-                }
-            } receiveValue: { [unowned self] result in
-                self.imageArray[item.id] = result
-                if self.imageArray.count == self.dishArray?.dishes.count {
-                    updateTableState.send(true)
-                }
-            }.store(in: &cancellable)
-        }
+        view.addSubview(buyButton)
+        NSLayoutConstraint.activate([
+            buyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buyButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.frame.height/2.8),
+            buyButton.widthAnchor.constraint(equalToConstant: view.frame.width - 40),
+            buyButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
     
-    //MARK: - getDishSorted
-    func getDishSorted() -> DishArray {
-        var dishSort: DishArray = .init(dishes: [])
-        
-        if let dishArray = dishArray?.dishes {
-            for item in dishArray {
-                for j in item.tegs {
-                    if j == sortDescriptor {
-                        dishSort.dishes.append(item)
-                    }
-                }
-            }
-        }
-        return dishSort
-    }
-    
-    //MARK: - appendDish
-    func appendDish(dish: Dish, image: Data, key: String) {
-        let dishModelForSave = DishModelForSave(name: dish.name, count: 1, price: dish.price, weight: dish.price, image: image)
-        if var arrayLoad = UserDefaults.standard.load(arrayForKey: key) as [DishModelForSave]? {
-            if arrayLoad.isEmpty {
-                UserDefaults.standard.save(array: [dishModelForSave], forKey: key)
-            } else {
-                if arrayLoad.contains(where: {$0.name == dishModelForSave.name}) {
-                    let index = arrayLoad.firstIndex(where: {$0.name == dishModelForSave.name}) ?? 0
-                    arrayLoad[index].count += 1
-                } else {
-                    arrayLoad.append(dishModelForSave)
-                }
-                UserDefaults.standard.save(array: arrayLoad, forKey: key)
-            }
+    private func configurateButton() {
+        if self.viewModel.getModel().count == .zero {
+            self.buyButton.isHidden = true
         } else {
-            UserDefaults.standard.save(array: [dishModelForSave], forKey: key)
+            self.buyButton.isHidden = false
+            self.buyButton.setTitle("Оплатить \(self.viewModel.returnTotalPrice()) ₽", for: .normal)
         }
     }
     
+}
+
+extension BasketViewController: UITableViewDelegate,
+                                UITableViewDataSource,
+                                CustomStepperDelegateProtocol {
+    func decrease(tag: Int) {
+        self.viewModel.removeModel(index: tag)
+    }
+    
+    func increaseButton(tag: Int) {
+        self.viewModel.appendModel(index: tag)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel.getModel().count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: BasketCell.identifier) as?
+            BasketCell {
+            
+            let dish = self.viewModel.getModel()
+            cell.stepper.tag = indexPath.row
+            cell.selectionStyle = .none
+            cell.config(image: dish[indexPath.row].image,
+                        name: dish[indexPath.row].name,
+                        weight: dish[indexPath.row].weight,
+                        price: dish[indexPath.row].price,
+                        count: dish[indexPath.row].count)
+            cell.stepper.delegate = self
+            
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
 }
