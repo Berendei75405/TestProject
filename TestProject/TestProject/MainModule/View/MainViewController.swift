c410d5c0eeb0a4995363a1de0c0f7f9808591fdc
Novgorodtcev <82874611+Berendei75405@users.noreply.github.com>
2023-07-03 17:42:20 +0700

    Update MainViewController.swift

--- TestProject/TestProject/MainModule/View/MainViewController.swift
+++ TestProject/TestProject/MainModule/View/MainViewController.swift
@@ -101,7 +101,10 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 extension UIViewController {
     func setNavigationBarMain() {
         let viewTitle = UIView(frame: .zero)
-        viewTitle.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 44)
+        viewTitle.frame = CGRect(x: .zero,
+                                 y: .zero,
+                                 width: view.frame.width,
+                                 height: 44)
         viewTitle.clipsToBounds = true
         
         let currentDate = Date()
@@ -111,20 +114,33 @@ extension UIViewController {
         let dateString = formatter.string(from: currentDate).capitalized
         
         let imageView = UIImageView(image: UIImage(named: "location"))
-        imageView.frame = CGRect(x: 0, y: 5, width: 30, height: 20)
+        imageView.frame = CGRect(x: .zero,
+                                 y: 5,
+                                 width: 30,
+                                 height: 20)
         imageView.contentMode = .scaleAspectFit
         
-        let cityLabel = UILabel(frame: CGRect(x: 30, y: 1, width: 200, height: 20))
+        let cityLabel = UILabel(frame: CGRect(x: 30,
+                                              y: 1,
+                                              width: 200,
+                                              height: 20))
         cityLabel.text = "Санкт-Петербург"
         cityLabel.textColor = .black
         cityLabel.font = .boldSystemFont(ofSize: 18)
         
-        let dateLabel = UILabel(frame: CGRect(x: 30, y: 20, width: 150, height: 20))
+        let dateLabel = UILabel(frame: CGRect(x: 30,
+                                              y: 20,
+                                              width: 150,
+                                              height: 20))
         dateLabel.text = dateString
         dateLabel.font = .systemFont(ofSize: 14)
         dateLabel.textColor = .gray
         
-        let profileImageView = UIImageView(frame: CGRect(x: view.frame.width - view.frame.width/5, y: -2, width: 44, height: 44))
+        let profileImageView = UIImageView(frame: CGRect(x:
+                                                            view.frame.width - view.frame.width/5,
+                                                         y: -2,
+                                                         width: 44,
+                                                         height: 44))
         profileImageView.image = UIImage(named: "face")
         profileImageView.layer.cornerRadius = 22
         profileImageView.clipsToBounds = true
